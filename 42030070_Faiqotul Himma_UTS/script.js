4da4337b9a31d58e150ff1b3471db11112284600
faiq16 <93690131+faiq16@users.noreply.github.com>
2022-04-12 21:10:49 +0700

    add file

---
+++ 42030070_Faiqotul Himma_UTS/script.js
@@ -0,0 +1,18 @@
+//Elemen => Cursor
+const Elemen = document.getElementById("Elemen");
+
+
+document.addEventListener("mousemove", e => {
+    
+    Elemen.setAttribute("style", "top: " + e.pageY + "px; left: " + e.pageX + "px;")
+});
+
+// Random Color Click Elemen
+function random(cal) {
+    return Math.floor(Math.random() * cal);
+}
+
+document.onclick  = function () { 
+randomColor = "rgb(" + random(255) + "," + random(255) + "," + random(255) + ")";
+Elemen.style.background = randomColor;
+} 
